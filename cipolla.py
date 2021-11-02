33057b08e131e54d111db5750df88c05dbc1690f
Elvir Vlahovljak <46258494+ElvaRivle@users.noreply.github.com>
2021-11-02 21:05:50 +0100

    Update cipolla.py

--- cipolla.py
+++ cipolla.py
@@ -1,4 +1,4 @@
-nadjeni = [-1]*1000
+found = [-1]*1000
 
 def cipolla(i, a, p):
     if (i == 1): 
@@ -6,20 +6,20 @@ def cipolla(i, a, p):
     elif (i == 2):
         return v2
 
-    if (nadjeni[i] != -1):
-        return nadjeni[i]
+    if (found[i] != -1):
+        return found[i]
     ret = 0
 
     if (i%2 == 0):
-        prosli = cipolla(i//2, a, p)
-        ret = ((prosli**2 % p) - 2*(a**(i//2) % p)) % p
+        last = cipolla(i//2, a, p)
+        ret = ((last**2 % p) - 2*(a**(i//2) % p)) % p
     else:
-        prosli1 = cipolla((i-1)//2, a, p)
-        prosli2 = cipolla((i+1)//2, a, p)
-        ret = ((prosli1*prosli2) % p - v1*(a**((i-1)//2) % p)) % p
+        last1 = cipolla((i-1)//2, a, p)
+        last2 = cipolla((i+1)//2, a, p)
+        ret = ((last1*last2) % p - v1*(a**((i-1)//2) % p)) % p
 
     print(f"v{i} = {ret}")
-    nadjeni[i] = ret
+    found[i] = ret
     return ret
 
 if __name__ == "__main__":
@@ -28,6 +28,6 @@ if __name__ == "__main__":
     v2 = int(input("v2 = "))
     a = int(input("a = "))
     p = int(input("p = "))
-    dokle = int((p+1)/2)
+    until = (p+1)//2
     print("\n")
-    cipolla(dokle, a, p)
+    cipolla(until, a, p)
